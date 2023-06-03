a8197897d802eeef234eba5d44085b48695ac80a
Luciano Mammino <lucianomammino@gmail.com>
2023-06-03 19:30:46 +0200

    Added example with customised retry strategy

--- examples/customise_client.rs
+++ examples/customise_client.rs
@@ -1,11 +1,8 @@
 use lastfm::ClientBuilder;
-use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api_key = env::var("LASTFM_API_KEY")?;
-
-    let client = ClientBuilder::new(api_key, "loige")
+    let client = ClientBuilder::new("some-api-key", "loige")
         .reqwest_client(reqwest::Client::new())
         .base_url("http://localhost:8080".parse().unwrap())
         .build();
